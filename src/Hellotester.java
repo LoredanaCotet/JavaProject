cb8370628589315e637cb704224c12be2d010344
Loredana <loredanadamian@gmail.com>
2023-06-09 11:41:43 +0200

    my first commit

---
+++ src/Hellotester.java
@@ -0,0 +1,32 @@
+
+package part1;
+
+import java.util.Scanner;
+
+public class Hellotester
+{
+    public static void main(String[] args) {
+
+        //1.Initialize what we know
+        int assignment_total=40;
+        int exam_total=200;
+        int assignment_grade;
+        int exam_grade;
+        //2.Get input
+        Scanner input=new Scanner (System.in);
+        System.out.println("Please enter total assignments grade:");
+        assignment_grade=input.nextInt();
+        System.out.println("Please your final exam grade");
+        exam_grade=input.nextInt();
+        //3.Do some processing
+        int student_grade=assignment_grade+exam_grade;
+        int total=assignment_total+exam_total;
+        float percentage=(float)student_grade/total*100;
+
+        //4.Print output
+        System.out.println("Your total grade is:" + student_grade);
+        System.out.println("Your percentage is:" + percentage);
+
+
+    }
+}
